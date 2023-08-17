276f4ccda3773ee86fcf709cedef5bff5c522fc9
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-08-17 14:01:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,5 +11,53 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/9",
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
+function fetchWithTime(apiUrl) {
+  const startTime = Date.now();
+  return fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const endTime = Date.now();
+      return { data, timeTaken: endTime - startTime };
+    });
+}
+
+async function fetchWithPromiseAll() {
+  const promises = apiUrls.map(apiUrl => fetchWithTime(apiUrl));
+  const results = await Promise.all(promises);
+  return results;
+}
+
+async function fetchWithPromiseAny() {
+  const promises = apiUrls.map(apiUrl => fetchWithTime(apiUrl));
+  try {
+    const result = await Promise.any(promises);
+    return [result];
+  } catch (error) {
+    return error.errors;
+  }
+}
+
+async function displayResults() {
+  const outputAllTable = document.getElementById('output-all');
+  const outputAnyTable = document.getElementById('output-any');
+
+  const resultsAll = await fetchWithPromiseAll();
+  const resultsAny = await fetchWithPromiseAny();
+
+  resultsAll.forEach(result => {
+    const row = outputAllTable.insertRow();
+    row.insertCell(0).textContent = result.data.url;
+    row.insertCell(1).textContent = result.timeTaken;
+  });
+
+  resultsAny.forEach(result => {
+    const row = outputAnyTable.insertRow();
+    row.insertCell(0).textContent = result.data.url;
+    row.insertCell(1).textContent = result.timeTaken;
+  });
+}
+
+displayResults();
+
 
 // You can write your code here
